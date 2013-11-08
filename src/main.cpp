c8415ae04499c2a020f144dc82ff9373bc70b7a5
Michał Dutkiewicz <emdeck@gmail.com>
2013-11-08 18:28:05 +0100

    Install translators

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,25 @@
 #include "ui/MainWindow.h"
 
+#include <QtCore/QLocale>
+#include <QtCore/QTranslator>
+#include <QtCore/QLibraryInfo>
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
+	QTranslator qtTranslator;
+	qtTranslator.load("qt_" + QLocale::system().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+
+	QTranslator applicationTranslator;
+	applicationTranslator.load(":/translations/otter_" + QLocale::system().name());
+
 	QApplication application(argc, argv);
 	application.setApplicationName("Otter");
 	application.setApplicationVersion("0.0.01");
 	application.setOrganizationName("eSoftware");
 	application.setOrganizationDomain("otter.emdek.pl");
+	application.installTranslator(&qtTranslator);
+	application.installTranslator(&applicationTranslator);
 
 	Otter::MainWindow window;
 	window.show();
