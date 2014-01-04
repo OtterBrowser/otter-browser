OtterBrowser/otter-browser

commit 017724538f0899a5bbd9603dde79d4760e7b3376

Fix segfault after deleting bookmark in subfolder

src/core/BookmarksManager.cpp
@@ -326,6 +326,7 @@ bool BookmarksManager::deleteBookmark(BookmarkInformation *bookmark, bool notify
 
 	m_bookmarks.removeAll(bookmark);
 	m_allBookmarks.removeAll(bookmark);
+	m_pointers[folder]->children.removeAll(bookmark);
 
 	if (bookmark->type == FolderBookmark)
 	{

commit 7ed8148e5e5e896db4aa57eb4c82239d58362b23

Correctly destroy BookmarksManager
This fixes a crash introduced in commit 017724538f

src/core/BookmarksManager.cpp
@@ -44,7 +44,7 @@ BookmarksManager::~BookmarksManager()
 {
 	for (int i = 0; i < m_bookmarks.count(); ++i)
 	{
-		deleteBookmark(m_bookmarks.at(i), false);
+		delete m_bookmarks.at(i);
 	}
 }
 