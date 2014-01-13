8d5610f6b45e283c12cdf3ba16b72b84e5b59d36
Michał Dutkiewicz <emdeck@gmail.com>
2014-01-13 16:55:26 +0100

    Do not ignore URL(s) passed through command line, fixes #64

--- src/main.cpp
+++ src/main.cpp
@@ -35,20 +35,25 @@ int main(int argc, char *argv[])
 	QCommandLineParser *parser = application.getParser();
 	parser->process(application);
 
+	Otter::MainWindow *window = NULL;
 	const QString session = ((Otter::SessionsManager::getCurrentSession().isEmpty() || !parser->value(QLatin1String("session")).isEmpty()) ? (parser->value(QLatin1String("session")).isEmpty() ? QLatin1String("default") : parser->value(QLatin1String("session"))) : QString());
 
 	if (session.isEmpty() || !Otter::SessionsManager::restoreSession(session))
 	{
-		Otter::MainWindow *window = application.createWindow(parser->isSet(QLatin1String("privatesession")));
+		 window = application.createWindow(parser->isSet(QLatin1String("privatesession")));
+	}
+	else
+	{
+		window = application.getWindow();
+	}
 
-		if (window && !parser->positionalArguments().isEmpty())
+	if (window && !parser->positionalArguments().isEmpty())
+	{
+		QStringList urls = parser->positionalArguments();
+
+		for (int i = 0; i < urls.count(); ++i)
 		{
-			QStringList urls = parser->positionalArguments();
-
-			for (int i = 0; i < urls.count(); ++i)
-			{
-				window->openUrl(QUrl(urls.at(i)));
-			}
+			window->openUrl(QUrl(urls.at(i)));
 		}
 	}
 
