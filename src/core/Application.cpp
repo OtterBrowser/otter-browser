52b11e8cb1edb63f83e7bf0187872c3b930f8d45
Michał Dutkiewicz <emdeck@gmail.com>
2014-04-01 22:09:32 +0200

    Start new development cycle

--- src/core/Application.cpp
+++ src/core/Application.cpp
@@ -49,7 +49,7 @@ Application::Application(int &argc, char **argv) : QApplication(argc, argv),
 	m_localServer(NULL)
 {
 	setApplicationName(QLatin1String("Otter"));
-	setApplicationVersion(QLatin1String("0.4.01"));
+	setApplicationVersion(QLatin1String("0.5.01-dev"));
 
 	m_instance = this;
 
