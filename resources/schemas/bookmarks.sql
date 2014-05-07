15a44c6cf833bd805affa5b0d04892224a8a9f82
Michał Dutkiewicz <emdeck@gmail.com>
2014-05-07 22:35:55 +0200

    Remove unused file

--- resources/schemas/bookmarks.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE "bookmarks" ("id" INTEGER PRIMARY KEY, "parent" INTEGER NOT NULL, "type" INTEGER NOT NULL, "address" TEXT NOT NULL, "keyword" TEXT UNIQUE, "title" TEXT, "description" TEXT, "order" INTEGER NOT NULL, "created" INTEGER NOT NULL, "visited" INTEGER NOT NULL DEFAULT -1, "visits" INTEGER NOT NULL DEFAULT 0);
-CREATE TABLE "tags" ("id" INTEGER PRIMARY KEY, "tag" TEXT NOT NULL UNIQUE);
-CREATE TABLE "bookmarks_tags" ("bookmark" INTEGER NOT NULL, "tag" INTEGER NOT NULL, PRIMARY KEY("bookmark", "tag"));
