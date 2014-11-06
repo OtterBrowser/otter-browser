ada3095516eb2d983a5e1da12bedf879769b989f
Michał Dutkiewicz <emdeck@gmail.com>
2014-11-06 21:43:41 +0100

    Fix Clone Tab action

--- src/ui/MainWindow.cpp
+++ src/ui/MainWindow.cpp
@@ -175,6 +175,7 @@ MainWindow::MainWindow(bool isPrivate, const SessionMainWindow &windows, QWidget
 	connect(m_actionsManager->getAction(QLatin1String("NewWindow")), SIGNAL(triggered()), this, SIGNAL(requestedNewWindow()));
 	connect(m_actionsManager->getAction(QLatin1String("NewWindowPrivate")), SIGNAL(triggered()), this, SLOT(actionNewWindowPrivate()));
 	connect(m_actionsManager->getAction(QLatin1String("Open")), SIGNAL(triggered()), this, SLOT(actionOpen()));
+	connect(m_actionsManager->getAction(QLatin1String("CloneTab")), SIGNAL(triggered()), m_windowsManager, SLOT(clone()));
 	connect(m_actionsManager->getAction(QLatin1String("CloseTab")), SIGNAL(triggered()), m_windowsManager, SLOT(close()));
 	connect(m_actionsManager->getAction(QLatin1String("SaveSession")), SIGNAL(triggered()), this, SLOT(actionSaveSession()));
 	connect(m_actionsManager->getAction(QLatin1String("ManageSessions")), SIGNAL(triggered()), this, SLOT(actionManageSessions()));
