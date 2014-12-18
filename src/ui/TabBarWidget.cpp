OtterBrowser/otter-browser

commit 70a3e4016ef0a73fe01967b24c57555cbcce3d09

Allow to disable URL icons in tab bar

src/ui/TabBarWidget.cpp
@@ -48,6 +48,7 @@ TabBarWidget::TabBarWidget(QWidget *parent) : QTabBar(parent),
 	m_clickedTab(-1),
 	m_hoveredTab(-1),
 	m_previewTimer(0),
+	m_showUrlIcon(true),
 	m_enablePreviews(true),
 	m_isMoved(false)
 {
@@ -64,6 +65,7 @@ TabBarWidget::TabBarWidget(QWidget *parent) : QTabBar(parent),
 	m_iconButtonPosition = ((m_closeButtonPosition == QTabBar::RightSide) ? QTabBar::LeftSide : QTabBar::RightSide);
 
 	optionChanged(QLatin1String("TabBar/ShowCloseButton"), SettingsManager::getValue(QLatin1String("TabBar/ShowCloseButton")));
+	optionChanged(QLatin1String("TabBar/ShowUrlIcon"), SettingsManager::getValue(QLatin1String("TabBar/ShowUrlIcon")));
 	optionChanged(QLatin1String("TabBar/EnablePreviews"), SettingsManager::getValue(QLatin1String("TabBar/EnablePreviews")));
 
 	connect(SettingsManager::getInstance(), SIGNAL(valueChanged(QString,QVariant)), this, SLOT(optionChanged(QString,QVariant)));
@@ -244,10 +246,18 @@ void TabBarWidget::tabInserted(int index)
 {
 	QTabBar::tabInserted(index);
 
-	QLabel *label = new QLabel();
-	label->setFixedSize(QSize(16, 16));
+	if (m_showUrlIcon)
+	{
+		QLabel *label = new QLabel();
+		label->setFixedSize(QSize(16, 16));
 
-	setTabButton(index, m_iconButtonPosition, label);
+		setTabButton(index, m_iconButtonPosition, label);
+	}
+	else
+	{
+		setTabButton(index, m_iconButtonPosition, NULL);
+	}
+
 	updateTabs();
 }
 
@@ -290,7 +300,6 @@ void TabBarWidget::addTab(int index, Window *window)
 	connect(window, SIGNAL(iconChanged(QIcon)), this, SLOT(updateTabs()));
 	connect(window, SIGNAL(loadingStateChanged(WindowLoadingState)), this, SLOT(updateTabs()));
 	connect(window, SIGNAL(isPinnedChanged(bool)), this, SLOT(updatePinnedTabsAmount()));
-	connect(tabButton(index, QTabBar::LeftSide), SIGNAL(destroyed()), window, SLOT(deleteLater()));
 
 	if (window->isPinned())
 	{
@@ -421,6 +430,32 @@ void TabBarWidget::optionChanged(const QString &option, const QVariant &value)
 	if (option == QLatin1String("TabBar/ShowCloseButton"))
 	{
 		setTabsClosable(value.toBool());
+	}
+	else if (option == QLatin1String("TabBar/ShowUrlIcon"))
+	{
+		if (m_showUrlIcon != value.toBool())
+		{
+			const bool showUrlIcons = value.toBool();
+
+			for (int i = 0; i < count(); ++i)
+			{
+				if (showUrlIcons && !tabButton(i, m_iconButtonPosition))
+				{
+					QLabel *label = new QLabel();
+					label->setFixedSize(QSize(16, 16));
+
+					setTabButton(i, m_iconButtonPosition, label);
+				}
+				else if (!showUrlIcons && tabButton(i, m_iconButtonPosition))
+				{
+					setTabButton(i, m_iconButtonPosition, NULL);
+				}
+			}
+
+			updateTabs();
+		}
+
+		m_showUrlIcon = value.toBool();
 	}
 	else if (option == QLatin1String("TabBar/EnablePreviews"))
 	{

resources/schemas/options.ini
@@ -441,3 +441,7 @@ value=true
 [TabBar/ShowCloseButton]
 type=bool
 value=true
+
+[TabBar/ShowUrlIcon]
+type=bool
+value=true

src/ui/TabBarWidget.h
@@ -88,6 +88,7 @@ private:
 	int m_clickedTab;
 	int m_hoveredTab;
 	int m_previewTimer;
+	bool m_showUrlIcon;
 	bool m_enablePreviews;
 	bool m_isMoved;
 

commit f8f2a00370aef3def1c143bc382e6f498e1a4d2b

Properly destroy Window when closing tab, fixes regression introduced in 70a3e4016ef0a73fe01967b24c57555cbcce3d09

src/ui/TabBarWidget.cpp
@@ -321,6 +321,13 @@ void TabBarWidget::removeTab(int index)
 		m_tabSize = (isHorizontal ? size.width() : size.height());
 	}
 
+	Window *window = qvariant_cast<Window*>(tabData(index));
+
+	if (window)
+	{
+		window->deleteLater();
+	}
+
 	QTabBar::removeTab(index);
 
 	if (underMouse() && tabAt(mapFromGlobal(QCursor::pos())) < 0)