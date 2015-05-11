34909d680e0060d38999fe230f585d4cc2f5ddca
Michał Dutkiewicz <emdeck@gmail.com>
2015-05-11 23:35:52 +0200

    Do not hide new windows, references #212

--- src/ui/WorkspaceWidget.cpp
+++ src/ui/WorkspaceWidget.cpp
@@ -77,6 +77,7 @@ void WorkspaceWidget::addWindow(Window *window)
 		if (m_mdi)
 		{
 			QMdiSubWindow *subWindow = m_mdi->addSubWindow(window, Qt::SubWindow);
+			subWindow->showNormal();
 			subWindow->setSystemMenu(NULL);
 			subWindow->installEventFilter(this);
 
