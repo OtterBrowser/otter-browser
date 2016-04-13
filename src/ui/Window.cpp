86dfc7d22d147ab75565cd1aa3582869f8b8b811
Michał Dutkiewicz <emdeck@gmail.com>
2016-04-13 22:08:37 +0200

    Set creator name for printing, fixes #1121

--- src/ui/Window.cpp
+++ src/ui/Window.cpp
@@ -171,6 +171,8 @@ void Window::triggerAction(int identifier, const QVariantMap &parameters)
 		case ActionsManager::PrintAction:
 			{
 				QPrinter printer;
+				printer.setCreator(QStringLiteral("Otter Browser %1").arg(Application::getInstance()->getFullVersion()));
+
 				QPrintDialog printDialog(&printer, this);
 				printDialog.setWindowTitle(tr("Print Page"));
 
@@ -186,6 +188,7 @@ void Window::triggerAction(int identifier, const QVariantMap &parameters)
 		case ActionsManager::PrintPreviewAction:
 			{
 				QPrintPreviewDialog printPreviewDialog(this);
+				printPreviewDialog.printer()->setCreator(QStringLiteral("Otter Browser %1").arg(Application::getInstance()->getFullVersion()));
 				printPreviewDialog.setWindowFlags(printPreviewDialog.windowFlags() | Qt::WindowMaximizeButtonHint | Qt::WindowMinimizeButtonHint);
 				printPreviewDialog.setWindowTitle(tr("Print Preview"));
 
