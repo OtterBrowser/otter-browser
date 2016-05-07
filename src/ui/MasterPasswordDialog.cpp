abbe66db01cce5d07b411892b0bb33b05fcb7882
Michał Dutkiewicz <emdeck@gmail.com>
2016-05-07 21:04:03 +0200

    Declare dialog to set master password

---
+++ src/ui/MasterPasswordDialog.cpp
@@ -0,0 +1,48 @@
+/**************************************************************************
+* Otter Browser: Web browser controlled by the user, not vice-versa.
+* Copyright (C) 2016 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+**************************************************************************/
+
+#include "MasterPasswordDialog.h"
+
+#include "ui_MasterPasswordDialog.h"
+
+namespace Otter
+{
+
+MasterPasswordDialog::MasterPasswordDialog(QWidget *parent) : Dialog(parent),
+	m_ui(new Ui::MasterPasswordDialog)
+{
+	m_ui->setupUi(this);
+}
+
+MasterPasswordDialog::~MasterPasswordDialog()
+{
+	delete m_ui;
+}
+
+void MasterPasswordDialog::changeEvent(QEvent *event)
+{
+	QDialog::changeEvent(event);
+
+	if (event->type() == QEvent::LanguageChange)
+	{
+		m_ui->retranslateUi(this);
+	}
+}
+
+}
