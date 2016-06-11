6d8523df920af463d50a8ebba3e8ada09e9bb7ff
Lukas Middendorf <code@lukas-middendorf.de>
2016-06-11 10:34:31 +0200

    add /usr/share/myspell/{,dicts/} to search path for hunspell dictionaries
     -on Fedora Linux dictionary packages put files into /usr/share/myspell/
     -the hunspell binary on Fedora also searches for dictionaries in /usr/share/myspell/dicts/

--- 3rdparty/sonnet/src/plugins/hunspell/hunspellclient.cpp
+++ 3rdparty/sonnet/src/plugins/hunspell/hunspellclient.cpp
@@ -57,7 +57,7 @@ HunspellClient::HunspellClient(QObject *parent)
 #ifdef Q_OS_MAC
     directories << QLatin1String("/System/Library/Spelling/");
 #else
-    directories << QLatin1String("/usr/share/hunspell/") << QLatin1String("/usr/local/share/hunspell/") << QLatin1String("/usr/local/share/mozilla-dicts/");
+    directories << QLatin1String("/usr/share/hunspell/") << QLatin1String("/usr/local/share/hunspell/") << QLatin1String("/usr/share/myspell/") << QLatin1String("/usr/share/myspell/dicts/") << QLatin1String("/usr/local/share/mozilla-dicts/");
 #endif
 
     const QString otterDirectory(qgetenv("OTTER_DICTIONARIES"));
