8f6020e6153088e6b6b1ff0ad63b75101630881b
Michał Dutkiewicz <emdeck@gmail.com>
2017-01-29 14:36:55 +0100

    Introduce CookiePropertiesDialog, references #1055

---
+++ src/ui/CookiePropertiesDialog.cpp
@@ -0,0 +1,94 @@
+/**************************************************************************
+* Otter Browser: Web browser controlled by the user, not vice-versa.
+* Copyright (C) 2017 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+**************************************************************************/
+
+#include "CookiePropertiesDialog.h"
+
+#include "ui_CookiePropertiesDialog.h"
+
+namespace Otter
+{
+
+CookiePropertiesDialog::CookiePropertiesDialog(const QNetworkCookie &cookie, QWidget *parent) : Dialog(parent),
+	m_cookie(cookie),
+	m_ui(new Ui::CookiePropertiesDialog)
+{
+	m_ui->setupUi(this);
+
+	if (cookie.name().isEmpty())
+	{
+		setWindowTitle(tr("Add Cookie"));
+	}
+	else
+	{
+		setWindowTitle(tr("Edit Cookie"));
+	}
+
+	m_ui->nameLineEdit->setText(QString(cookie.name()));
+	m_ui->valueLineEdit->setText(QString(cookie.value()));
+	m_ui->domainLineEdit->setText(cookie.domain());
+	m_ui->pathLineEdit->setText(cookie.path());
+	m_ui->isSessionOnlyCheckBox->setChecked(!cookie.expirationDate().isValid());
+	m_ui->expiresDateTimeEdit->setDateTime(cookie.expirationDate());
+	m_ui->expiresDateTimeEdit->setEnabled(cookie.expirationDate().isValid());
+	m_ui->isSecureCheckBox->setChecked(cookie.isSecure());
+	m_ui->isHttpOnlyCheckBox->setChecked(cookie.isHttpOnly());
+
+	connect(m_ui->isSessionOnlyCheckBox, SIGNAL(clicked(bool)), m_ui->expiresDateTimeEdit, SLOT(setDisabled(bool)));
+}
+
+CookiePropertiesDialog::~CookiePropertiesDialog()
+{
+	delete m_ui;
+}
+
+void CookiePropertiesDialog::changeEvent(QEvent *event)
+{
+	QDialog::changeEvent(event);
+
+	if (event->type() == QEvent::LanguageChange)
+	{
+		m_ui->retranslateUi(this);
+	}
+}
+
+QNetworkCookie CookiePropertiesDialog::getOriginalCookie() const
+{
+	return m_cookie;
+}
+
+QNetworkCookie CookiePropertiesDialog::getModifiedCookie() const
+{
+	QNetworkCookie cookie(m_cookie);
+	cookie.setName(m_ui->nameLineEdit->text().toUtf8());
+	cookie.setValue(m_ui->valueLineEdit->text().toUtf8());
+	cookie.setDomain(m_ui->domainLineEdit->text());
+	cookie.setPath(m_ui->pathLineEdit->text());
+	cookie.setExpirationDate(m_ui->isSessionOnlyCheckBox->isChecked() ? QDateTime() : m_ui->expiresDateTimeEdit->dateTime());
+	cookie.setSecure(m_ui->isSecureCheckBox->isChecked());
+	cookie.setHttpOnly(m_ui->isHttpOnlyCheckBox->isChecked());
+
+	return cookie;
+}
+
+bool CookiePropertiesDialog::isModified() const
+{
+	return (m_cookie != getModifiedCookie());
+}
+
+}
---
+++ src/ui/CookiePropertiesDialog.h
@@ -0,0 +1,57 @@
+/**************************************************************************
+* Otter Browser: Web browser controlled by the user, not vice-versa.
+* Copyright (C) 2017 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+**************************************************************************/
+
+#ifndef OTTER_COOKIEPROPERTIESDIALOG_H
+#define OTTER_COOKIEPROPERTIESDIALOG_H
+
+#include "Dialog.h"
+
+#include <QtNetwork/QNetworkCookie>
+
+namespace Otter
+{
+
+namespace Ui
+{
+	class CookiePropertiesDialog;
+}
+
+class CookiePropertiesDialog : public Dialog
+{
+	Q_OBJECT
+
+public:
+	explicit CookiePropertiesDialog(const QNetworkCookie &cookie, QWidget *parent = nullptr);
+	~CookiePropertiesDialog();
+
+	QNetworkCookie getOriginalCookie() const;
+	QNetworkCookie getModifiedCookie() const;
+	bool isModified() const;
+
+protected:
+	void changeEvent(QEvent *event);
+
+private:
+	QNetworkCookie m_cookie;
+	Ui::CookiePropertiesDialog *m_ui;
+};
+
+}
+
+#endif
