9f1f468338a22248abee52fa79b659bac793092b
Michał Dutkiewicz <emdeck@gmail.com>
2017-04-01 22:22:34 +0200

    Cleanup whitespace

--- src/modules/backends/web/qtwebengine/resources/hideBlockedRequests.js
+++ src/modules/backends/web/qtwebengine/resources/hideBlockedRequests.js
@@ -1,13 +1,13 @@
-﻿var requests = [%1];
+var requests = [%1];
 var elements = Array.from(document.querySelectorAll('[src]'));
 
 for (var i = 0; i < elements.length; ++i)
 {
-    for (var j = 0; j < requests.length; ++j)
-    {
-        if (elements[i].getAttribute('src').indexOf(requests[j]) > -1)
-        {
-            elements[i].style.cssText = 'display:none !important';
-        }
-    }
+	for (var j = 0; j < requests.length; ++j)
+	{
+		if (elements[i].getAttribute('src').indexOf(requests[j]) > -1)
+		{
+			elements[i].style.cssText = 'display:none !important';
+		}
+	}
 }
--- src/modules/backends/web/qtwebengine/resources/hideElements.js
+++ src/modules/backends/web/qtwebengine/resources/hideElements.js
@@ -4,38 +4,38 @@ var ignoredElements = [];
 
 for (var i = 0; i < whitelist.length; ++i)
 {
-    try
-    {
-        ignoredElements = ignoredElements.concat(Array.from(document.querySelectorAll(whitelist[i])));
-    }
-    catch (e)
-    {
-        console.error("Invalid selector: " + whitelist[i]);
+	try
+	{
+		ignoredElements = ignoredElements.concat(Array.from(document.querySelectorAll(whitelist[i])));
+	}
+	catch (e)
+	{
+		console.error("Invalid selector: " + whitelist[i]);
 
-        continue;
-    }
+		continue;
+	}
 }
 
 for (var i = 0; i < blacklist.length; ++i)
 {
-    var elements = [];
-     
-    try
-    {
-        elements = document.querySelectorAll(blacklist[i]);
-    }
-    catch (e)
-    {
-        console.error("Invalid selector: " + blacklist[i]);
+	var elements = [];
 
-        continue;
-    }
-     
-    for (var j = 0; j < elements.length; ++j)
-    {
-        if (ignoredElements.indexOf(elements[j]) < 0)
-        {
-            elements[j].style.cssText = "display:none !important";
-        }
-    }
+	try
+	{
+		elements = document.querySelectorAll(blacklist[i]);
+	}
+	catch (e)
+	{
+		console.error("Invalid selector: " + blacklist[i]);
+
+		continue;
+	}
+
+	for (var j = 0; j < elements.length; ++j)
+	{
+		if (ignoredElements.indexOf(elements[j]) < 0)
+		{
+			elements[j].style.cssText = "display:none !important";
+		}
+	}
 }
