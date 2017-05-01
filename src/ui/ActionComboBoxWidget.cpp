846940d2f67bccd9a509b64558dd6474b23f348c
Michał Dutkiewicz <emdeck@gmail.com>
2017-05-01 21:33:05 +0200

    Minor cleanup

--- src/ui/ActionComboBoxWidget.cpp
+++ src/ui/ActionComboBoxWidget.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2015 - 2016 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2015 - 2017 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -21,7 +21,6 @@
 #include "ItemViewWidget.h"
 #include "../core/ActionsManager.h"
 
-#include <QtCore/QCoreApplication>
 #include <QtCore/QTimer>
 #include <QtWidgets/QLineEdit>
 #include <QtWidgets/QStylePainter>
