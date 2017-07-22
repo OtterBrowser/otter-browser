6a8a53810c23dd0efbc7269bc175512e3664eb21
Michał Dutkiewicz <emdeck@gmail.com>
2017-07-22 21:03:48 +0200

    [QtWebEngine] Improve handling of DNT header

--- src/modules/backends/web/qtwebengine/QtWebEngineUrlRequestInterceptor.cpp
+++ src/modules/backends/web/qtwebengine/QtWebEngineUrlRequestInterceptor.cpp
@@ -92,77 +92,77 @@ void QtWebEngineUrlRequestInterceptor::interceptRequest(QWebEngineUrlRequestInfo
 
 	const QVector<int> contentBlockingProfiles(m_contentBlockingProfiles.value(request.firstPartyUrl().host()));
 
-	if (contentBlockingProfiles.isEmpty())
+	if (!contentBlockingProfiles.isEmpty())
 	{
-		const NetworkManagerFactory::DoNotTrackPolicy doNotTrackPolicy(NetworkManagerFactory::getDoNotTrackPolicy());
+		NetworkManager::ResourceType resourceType(NetworkManager::OtherType);
+		bool storeBlockedUrl(true);
 
-		if (doNotTrackPolicy != NetworkManagerFactory::SkipTrackPolicy)
+		switch (request.resourceType())
 		{
-			request.setHttpHeader(QStringLiteral("DNT").toLatin1(), ((doNotTrackPolicy == NetworkManagerFactory::DoNotAllowToTrackPolicy) ? QStringLiteral("1") : QStringLiteral("0")).toLatin1());
-		}
+			case QWebEngineUrlRequestInfo::ResourceTypeMainFrame:
+				resourceType = NetworkManager::MainFrameType;
 
-		return;
-	}
+				break;
+			case QWebEngineUrlRequestInfo::ResourceTypeSubFrame:
+				resourceType = NetworkManager::SubFrameType;
 
-	NetworkManager::ResourceType resourceType(NetworkManager::OtherType);
-	bool storeBlockedUrl(true);
+				break;
+			case QWebEngineUrlRequestInfo::ResourceTypeStylesheet:
+				resourceType = NetworkManager::StyleSheetType;
+				storeBlockedUrl = false;
 
-	switch (request.resourceType())
-	{
-		case QWebEngineUrlRequestInfo::ResourceTypeMainFrame:
-			resourceType = NetworkManager::MainFrameType;
+				break;
+			case QWebEngineUrlRequestInfo::ResourceTypeScript:
+				resourceType = NetworkManager::ScriptType;
+				storeBlockedUrl = false;
 
-			break;
-		case QWebEngineUrlRequestInfo::ResourceTypeSubFrame:
-			resourceType = NetworkManager::SubFrameType;
+				break;
+			case QWebEngineUrlRequestInfo::ResourceTypeImage:
+				resourceType = NetworkManager::ImageType;
 
-			break;
-		case QWebEngineUrlRequestInfo::ResourceTypeStylesheet:
-			resourceType = NetworkManager::StyleSheetType;
-			storeBlockedUrl = false;
+				break;
+			case QWebEngineUrlRequestInfo::ResourceTypeObject:
+			case QWebEngineUrlRequestInfo::ResourceTypeMedia:
+				resourceType = NetworkManager::ObjectType;
 
-			break;
-		case QWebEngineUrlRequestInfo::ResourceTypeScript:
-			resourceType = NetworkManager::ScriptType;
-			storeBlockedUrl = false;
-
-			break;
-		case QWebEngineUrlRequestInfo::ResourceTypeImage:
-			resourceType = NetworkManager::ImageType;
-
-			break;
-		case QWebEngineUrlRequestInfo::ResourceTypeObject:
-		case QWebEngineUrlRequestInfo::ResourceTypeMedia:
-			resourceType = NetworkManager::ObjectType;
-
-			break;
+				break;
 #if QT_VERSION >= 0x050700
-		case QWebEngineUrlRequestInfo::ResourceTypePluginResource:
-			resourceType = NetworkManager::ObjectSubrequestType;
-			storeBlockedUrl = false;
+			case QWebEngineUrlRequestInfo::ResourceTypePluginResource:
+				resourceType = NetworkManager::ObjectSubrequestType;
+				storeBlockedUrl = false;
 
-			break;
+				break;
 #endif
-		case QWebEngineUrlRequestInfo::ResourceTypeXhr:
-			resourceType = NetworkManager::XmlHttpRequestType;
+			case QWebEngineUrlRequestInfo::ResourceTypeXhr:
+				resourceType = NetworkManager::XmlHttpRequestType;
 
-			break;
-		default:
-			break;
-	}
-
-	const ContentBlockingManager::CheckResult result(ContentBlockingManager::checkUrl(contentBlockingProfiles, request.firstPartyUrl(), request.requestUrl(), resourceType));
-
-	if (result.isBlocked)
-	{
-		if (storeBlockedUrl && !m_blockedElements.value(request.firstPartyUrl().host()).contains(request.requestUrl().url()))
-		{
-			m_blockedElements[request.firstPartyUrl().host()].append(request.requestUrl().url());
+				break;
+			default:
+				break;
 		}
 
-		Console::addMessage(QCoreApplication::translate("main", "Request blocked with rule: %1").arg(result.rule), Console::NetworkCategory, Console::LogLevel, request.requestUrl().toString(), -1);
+		const ContentBlockingManager::CheckResult result(ContentBlockingManager::checkUrl(contentBlockingProfiles, request.firstPartyUrl(), request.requestUrl(), resourceType));
 
-		request.block(true);
+		if (result.isBlocked)
+		{
+			if (storeBlockedUrl && !m_blockedElements.value(request.firstPartyUrl().host()).contains(request.requestUrl().url()))
+			{
+				m_blockedElements[request.firstPartyUrl().host()].append(request.requestUrl().url());
+			}
+
+			Console::addMessage(QCoreApplication::translate("main", "Request blocked with rule: %1").arg(result.rule), Console::NetworkCategory, Console::LogLevel, request.requestUrl().toString(), -1);
+
+			request.block(true);
+
+			return;
+		}
+	}
+
+	const NetworkManagerFactory::DoNotTrackPolicy doNotTrackPolicy(NetworkManagerFactory::getDoNotTrackPolicy());
+
+	if (doNotTrackPolicy != NetworkManagerFactory::SkipTrackPolicy)
+	{
+		request.setHttpHeader(QStringLiteral("DNT").toLatin1(), ((doNotTrackPolicy == NetworkManagerFactory::DoNotAllowToTrackPolicy) ? QStringLiteral("1") : QStringLiteral("0")).toLatin1());
 	}
 }
 
