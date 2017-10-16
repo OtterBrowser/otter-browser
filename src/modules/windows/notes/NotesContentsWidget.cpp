e237ed678fe04a609859701084f7ba2f0d8d2b3e
Michał Dutkiewicz <emdeck@gmail.com>
2017-10-16 22:02:45 +0200

    Cleanup

--- src/modules/windows/notes/NotesContentsWidget.cpp
+++ src/modules/windows/notes/NotesContentsWidget.cpp
@@ -262,14 +262,14 @@ void NotesContentsWidget::triggerAction(int identifier, const QVariantMap &param
 
 void NotesContentsWidget::updateActions()
 {
-	const bool hasSelecion(!m_ui->notesViewWidget->selectionModel()->selectedIndexes().isEmpty());
 	const QModelIndex index(m_ui->notesViewWidget->getCurrentIndex());
 	const BookmarksModel::BookmarkType type(static_cast<BookmarksModel::BookmarkType>(index.data(BookmarksModel::TypeRole).toInt()));
+	const bool isUrlBookmark(type == BookmarksModel::UrlBookmark);
 
-	m_ui->addressLabelWidget->setText((type == BookmarksModel::UrlBookmark) ? index.data(BookmarksModel::UrlRole).toString() : QString());
-	m_ui->addressLabelWidget->setUrl((type == BookmarksModel::UrlBookmark) ? index.data(BookmarksModel::UrlRole).toUrl() : QUrl());
-	m_ui->dateLabelWidget->setText((type == BookmarksModel::UrlBookmark) ? Utils::formatDateTime(index.data(BookmarksModel::TimeAddedRole).toDateTime()) : QString());
-	m_ui->deleteButton->setEnabled(hasSelecion && type != BookmarksModel::RootBookmark && type != BookmarksModel::TrashBookmark);
+	m_ui->addressLabelWidget->setText(isUrlBookmark ? index.data(BookmarksModel::UrlRole).toString() : QString());
+	m_ui->addressLabelWidget->setUrl(isUrlBookmark ? index.data(BookmarksModel::UrlRole).toUrl() : QUrl());
+	m_ui->dateLabelWidget->setText(isUrlBookmark ? Utils::formatDateTime(index.data(BookmarksModel::TimeAddedRole).toDateTime()) : QString());
+	m_ui->deleteButton->setEnabled(!m_ui->notesViewWidget->selectionModel()->selectedIndexes().isEmpty() && type != BookmarksModel::RootBookmark && type != BookmarksModel::TrashBookmark);
 	m_ui->textEditWidget->blockSignals(true);
 	m_ui->textEditWidget->setPlainText(index.data(BookmarksModel::DescriptionRole).toString());
 	m_ui->textEditWidget->blockSignals(false);
