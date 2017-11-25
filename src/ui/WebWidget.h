16dc1ce4cd75383d2100e3285fad0d0fdfe836cb
Michał Dutkiewicz <emdeck@gmail.com>
2017-11-25 16:43:58 +0100

    Declare value for MIME type

--- src/ui/WebWidget.h
+++ src/ui/WebWidget.h
@@ -120,6 +120,7 @@ public:
 		DocumentBytesReceivedInformation,
 		DocumentBytesTotalInformation,
 		DocumentLoadingProgressInformation,
+		DocumentMimeTypeInformation,
 		TotalBytesReceivedInformation,
 		TotalBytesTotalInformation,
 		TotalLoadingProgressInformation,
