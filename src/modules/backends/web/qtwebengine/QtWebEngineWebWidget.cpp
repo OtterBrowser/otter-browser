52c38725e69b2273af85027000a020e16947d295
Michał Dutkiewicz <emdeck@gmail.com>
2017-12-03 19:31:20 +0100

    [QtWebEngine] Simplify

--- src/modules/backends/web/qtwebengine/QtWebEngineWebWidget.cpp
+++ src/modules/backends/web/qtwebengine/QtWebEngineWebWidget.cpp
@@ -828,7 +828,6 @@ void QtWebEngineWebWidget::triggerAction(int identifier, const QVariantMap &para
 						return;
 					}
 
-					const QUrlQuery parameters(result.toMap().value(QLatin1String("query")).toString());
 					const QStringList identifiers(SearchEnginesManager::getSearchEngines());
 					const QStringList keywords(SearchEnginesManager::getSearchKeywords());
 					const QIcon icon(getIcon());
@@ -841,7 +840,7 @@ void QtWebEngineWebWidget::triggerAction(int identifier, const QVariantMap &para
 					searchEngine.resultsUrl.url = (url.isEmpty() ? getUrl() : (url.isRelative() ? getUrl().resolved(url) : url)).toString();
 					searchEngine.resultsUrl.enctype = result.toMap().value(QLatin1String("enctype")).toString();
 					searchEngine.resultsUrl.method = result.toMap().value(QLatin1String("method")).toString();
-					searchEngine.resultsUrl.parameters = parameters;
+					searchEngine.resultsUrl.parameters = QUrlQuery(result.toMap().value(QLatin1String("query")).toString());
 
 					SearchEnginePropertiesDialog dialog(searchEngine, keywords, this);
 
