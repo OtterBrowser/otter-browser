095b3ec9af7bd69290761f1e69c6910c84cdf5e7
Michał Dutkiewicz <emdeck@gmail.com>
2017-12-14 20:38:38 +0100

    Simplify

--- src/modules/widgets/address/AddressWidget.cpp
+++ src/modules/widgets/address/AddressWidget.cpp
@@ -1111,13 +1111,11 @@ void AddressWidget::updateGeometries()
 
 				break;
 			case LoadPluginsEntry:
-				if (!m_window || m_window->isAboutToClose() || m_window->getLoadingState() != WebWidget::FinishedLoadingState || !m_window->getActionState(ActionsManager::LoadPluginsAction).isEnabled)
+				if (m_window && !m_window->isAboutToClose() && m_window->getLoadingState() == WebWidget::FinishedLoadingState && m_window->getActionState(ActionsManager::LoadPluginsAction).isEnabled)
 				{
-					continue;
+					definition.title = QT_TR_NOOP("Load all plugins on the page");
+					definition.icon = ThemesManager::createIcon(QLatin1String("preferences-plugin"), false);
 				}
-
-				definition.title = QT_TR_NOOP("Load all plugins on the page");
-				definition.icon = ThemesManager::createIcon(QLatin1String("preferences-plugin"), false);
 
 				break;
 			case FillPasswordEntry:
