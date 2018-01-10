896e99139a6c48c387eb2b55fbcbc28a408e30fa
Michał Dutkiewicz <emdeck@gmail.com>
2018-01-10 21:14:36 +0100

    [Windows] Switch to new API

--- src/modules/platforms/windows/WindowsPlatformIntegration.cpp
+++ src/modules/platforms/windows/WindowsPlatformIntegration.cpp
@@ -1,7 +1,7 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
 * Copyright (C) 2014 - 2016 Jan Bajer aka bajasoft <jbajer@gmail.com>
-* Copyright (C) 2016 - 2017 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2016 - 2018 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -132,7 +132,7 @@ void WindowsPlatformIntegration::updateTaskbarButtons()
 				m_taskbarButtons[mainWindow]->progress()->show();
 			}
 
-			m_taskbarButtons[mainWindow]->progress()->setValue((bytesReceived > 0) ? qFloor((static_cast<qreal>(bytesReceived) / bytesTotal) * 100) : 0);
+			m_taskbarButtons[mainWindow]->progress()->setValue((bytesReceived > 0) ? qFloor(Utils::calculatePercent(bytesReceived, bytesTotal)) : 0);
 		}
 		else if (m_taskbarButtons.contains(mainWindow))
 		{
