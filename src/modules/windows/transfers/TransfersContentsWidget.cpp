17efa576b838ac7b9bdd0feec21f9cd5da89bbe0
Michał Dutkiewicz <emdeck@gmail.com>
2018-01-11 23:05:10 +0100

    Update information widget on language change

--- src/modules/windows/transfers/TransfersContentsWidget.cpp
+++ src/modules/windows/transfers/TransfersContentsWidget.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2013 - 2017 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2013 - 2018 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -131,6 +131,8 @@ void TransfersContentsWidget::changeEvent(QEvent *event)
 		m_ui->retranslateUi(this);
 
 		m_model->setHorizontalHeaderLabels({tr("Status"), tr("Filename"), tr("Size"), tr("Progress"), tr("Time"), tr("Speed"), tr("Started"), tr("Finished")});
+
+		updateActions();
 	}
 }
 
