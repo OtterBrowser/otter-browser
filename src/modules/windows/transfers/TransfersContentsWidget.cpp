db1d6aecd68e7fbffd648280180822f8ab7c3408
Michał Dutkiewicz <emdeck@gmail.com>
2018-01-11 23:15:24 +0100

    Use existing method

--- src/modules/windows/transfers/TransfersContentsWidget.cpp
+++ src/modules/windows/transfers/TransfersContentsWidget.cpp
@@ -642,7 +642,7 @@ int TransfersContentsWidget::findTransfer(Transfer *transfer) const
 
 	for (int i = 0; i < m_model->rowCount(); ++i)
 	{
-		if (transfer == static_cast<Transfer*>(m_model->index(i, 0).data(Qt::UserRole).value<void*>()))
+		if (transfer == getTransfer(m_model->index(i, 0)))
 		{
 			return i;
 		}
