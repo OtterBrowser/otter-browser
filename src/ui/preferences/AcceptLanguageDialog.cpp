86862fc271d58b15ae3ed0a68cd16f34317b3066
Michał Dutkiewicz <emdeck@gmail.com>
2018-01-14 22:31:50 +0100

    Use helper variable

--- src/ui/preferences/AcceptLanguageDialog.cpp
+++ src/ui/preferences/AcceptLanguageDialog.cpp
@@ -1,7 +1,7 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
 * Copyright (C) 2014 Piotr Wójcik <chocimier@tlen.pl>
-* Copyright (C) 2015 - 2017 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2015 - 2018 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -208,11 +208,20 @@ QString AcceptLanguageDialog::getLanguages()
 
 bool AcceptLanguageDialog::eventFilter(QObject *object, QEvent *event)
 {
-	if (object == m_ui->languagesComboBox && event->type() == QEvent::KeyPress && (static_cast<QKeyEvent*>(event)->key() == Qt::Key_Enter || static_cast<QKeyEvent*>(event)->key() == Qt::Key_Return))
+	if (object == m_ui->languagesComboBox && event->type() == QEvent::KeyPress)
 	{
-		addNewLanguage();
+		const QKeyEvent *keyEvent(static_cast<QKeyEvent*>(event));
 
-		return true;
+		switch (keyEvent->key())
+		{
+			case Qt::Key_Enter:
+			case Qt::Key_Return:
+				addNewLanguage();
+
+				return true;
+			default:
+				break;
+		}
 	}
 
 	return false;
