5ac684b2c35e6ea366666989dac7b0fbc4b0e8d0
Michał Dutkiewicz <emdeck@gmail.com>
2018-01-14 22:41:07 +0100

    Use shorter notation

--- src/ui/ClearHistoryDialog.cpp
+++ src/ui/ClearHistoryDialog.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2013 - 2017 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2013 - 2018 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -38,7 +38,7 @@ ClearHistoryDialog::ClearHistoryDialog(const QStringList &clearSettings, bool co
 	m_ui->setupUi(this);
 
 	QStringList settings(clearSettings);
-	settings.removeAll(QString());
+	settings.removeAll({});
 
 	if (settings.isEmpty())
 	{
