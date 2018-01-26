9886b5dad7dc6bbf16bc92af9a575ca6ba99278b
Michał Dutkiewicz <emdeck@gmail.com>
2018-01-26 18:49:26 +0100

    Fix building against Qt 5.11, spotted by Chiitoo

--- src/ui/ReportDialog.cpp
+++ src/ui/ReportDialog.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2015 - 2017 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2015 - 2018 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -22,6 +22,7 @@
 
 #include "ui_ReportDialog.h"
 
+#include <QtCore/QRegularExpression>
 #include <QtGui/QClipboard>
 #include <QtGui/QFontDatabase>
 #include <QtWidgets/QPushButton>
