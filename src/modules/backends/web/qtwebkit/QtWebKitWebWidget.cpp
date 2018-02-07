08b7ac64215679d77c1bf90d7d826e5b2ce6f5a7
Michał Dutkiewicz <emdeck@gmail.com>
2018-02-07 22:43:30 +0100

    [QtWebKit] Use helper variable

--- src/modules/backends/web/qtwebkit/QtWebKitWebWidget.cpp
+++ src/modules/backends/web/qtwebkit/QtWebKitWebWidget.cpp
@@ -1640,8 +1640,9 @@ void QtWebKitWebWidget::triggerAction(int identifier, const QVariantMap &paramet
 				m_page->mainFrame()->setFocus();
 
 				const QWebElement element(m_page->mainFrame()->findFirstElement(QLatin1String(":focus")));
+				const QString tagName(element.tagName().toLower());
 
-				if (element.tagName().toLower() == QLatin1String("textarea") || element.tagName().toLower() == QLatin1String("input"))
+				if (tagName == QLatin1String("textarea") || tagName == QLatin1String("input"))
 				{
 					m_page->mainFrame()->documentElement().evaluateJavaScript(QLatin1String("document.activeElement.blur()"));
 				}
