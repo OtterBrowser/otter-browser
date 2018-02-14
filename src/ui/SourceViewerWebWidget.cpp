9ba8afc88914f431adfd51f80ad85a600945da72
Michał Dutkiewicz <emdeck@gmail.com>
2018-02-14 22:40:04 +0100

    Implement getScrollPosition()

--- src/ui/SourceViewerWebWidget.cpp
+++ src/ui/SourceViewerWebWidget.cpp
@@ -446,7 +446,7 @@ QIcon SourceViewerWebWidget::getIcon() const
 
 QPoint SourceViewerWebWidget::getScrollPosition() const
 {
-	return {};
+	return {m_sourceViewer->horizontalScrollBar()->value(), m_sourceViewer->verticalScrollBar()->value()};
 }
 
 ActionsManager::ActionDefinition::State SourceViewerWebWidget::getActionState(int identifier, const QVariantMap &parameters) const
