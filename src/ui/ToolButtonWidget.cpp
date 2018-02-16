92db0dd6eadad1e46ae48691bf588ae745d63937
Michał Dutkiewicz <emdeck@gmail.com>
2018-02-16 23:22:34 +0100

    Drop unused include

--- src/ui/ToolButtonWidget.cpp
+++ src/ui/ToolButtonWidget.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2015 - 2017 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2015 - 2018 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -23,7 +23,6 @@
 #include "ToolBarWidget.h"
 #include "../core/Application.h"
 #include "../core/ThemesManager.h"
-#include "../core/Utils.h"
 
 #include <QtCore/QEvent>
 #include <QtGui/QActionEvent>
