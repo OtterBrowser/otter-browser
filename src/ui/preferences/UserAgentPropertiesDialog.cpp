5804a925d07dac2d5bf1182ba41f7308879f70b3
Michał Dutkiewicz <emdeck@gmail.com>
2018-02-22 21:01:47 +0100

    Simplify

--- src/ui/preferences/UserAgentPropertiesDialog.cpp
+++ src/ui/preferences/UserAgentPropertiesDialog.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2017 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2017 - 2018 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -73,7 +73,7 @@ void UserAgentPropertiesDialog::insertPlaceholder(QAction *action)
 {
 	if (!action->data().toString().isEmpty())
 	{
-		m_ui->valueLineEditWidget->insert(QStringLiteral("{%1}").arg(action->data().toString()));
+		m_ui->valueLineEditWidget->insert(QLatin1Char('{') + action->data().toString() + QLatin1Char('}'));
 	}
 }
 
