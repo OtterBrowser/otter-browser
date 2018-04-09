a7ac7c84ac95b02da05f8db2b449adf32ae0942a
Michał Dutkiewicz <emdeck@gmail.com>
2018-04-09 21:03:36 +0200

    Check if index is valid

--- src/modules/windows/links/LinksContentsWidget.cpp
+++ src/modules/windows/links/LinksContentsWidget.cpp
@@ -311,12 +311,15 @@ bool LinksContentsWidget::eventFilter(QObject *object, QEvent *event)
 		const QModelIndex index(m_ui->linksViewWidget->indexAt(helpEvent->pos()));
 		QString toolTip;
 
-		if (index.data(Qt::DisplayRole).toString() != index.data(Qt::StatusTipRole).toUrl().toDisplayString(QUrl::RemovePassword))
+		if (index.isValid())
 		{
-			toolTip.append(tr("Title: %1").arg(index.data(Qt::DisplayRole).toString()) + QLatin1Char('\n'));
-		}
+			if (index.data(Qt::DisplayRole).toString() != index.data(Qt::StatusTipRole).toUrl().toDisplayString(QUrl::RemovePassword))
+			{
+				toolTip.append(tr("Title: %1").arg(index.data(Qt::DisplayRole).toString()) + QLatin1Char('\n'));
+			}
 
-		toolTip.append(tr("Address: %1").arg(index.data(Qt::StatusTipRole).toUrl().toDisplayString()));
+			toolTip.append(tr("Address: %1").arg(index.data(Qt::StatusTipRole).toUrl().toDisplayString()));
+		}
 
 		QToolTip::showText(helpEvent->globalPos(), QFontMetrics(QToolTip::font()).elidedText(toolTip, Qt::ElideRight, (QApplication::desktop()->screenGeometry(m_ui->linksViewWidget).width() / 2)), m_ui->linksViewWidget, m_ui->linksViewWidget->visualRect(index));
 
