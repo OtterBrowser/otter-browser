e3739f947389d4a4506c6d7f667862c4061a2e62
Michał Dutkiewicz <emdeck@gmail.com>
2018-05-28 20:22:55 +0200

    Implement basic Atom parser, references #1415

--- src/core/FeedParser.cpp
+++ src/core/FeedParser.cpp
@@ -18,6 +18,7 @@
 **************************************************************************/
 
 #include "FeedParser.h"
+#include "Console.h"
 #include "FeedsManager.h"
 #include "Job.h"
 
@@ -42,11 +43,124 @@ AtomFeedParser::AtomFeedParser(Feed *parent) : FeedParser(parent),
 
 bool AtomFeedParser::parse(DataFetchJob *data)
 {
-	Q_UNUSED(data)
+	QXmlStreamReader reader(data->getData());
+	QStringList categories;
+	QVector<Feed::Entry> entries;
+	entries.reserve(10);
 
-//TODO
+	if (reader.readNextStartElement() && reader.name() == QLatin1String("feed"))
+	{
+		while (reader.readNextStartElement())
+		{
+			if (reader.name() == QLatin1String("entry"))
+			{
+				Feed::Entry entry;
 
-	return false;
+				while (reader.readNext())
+				{
+					if (reader.isStartElement())
+					{
+						if (reader.name() == QLatin1String("category"))
+						{
+							entry.categories.append(reader.attributes().value(QLatin1String("term")).toString());
+
+							reader.skipCurrentElement();
+						}
+						else if (reader.name() == QLatin1String("title"))
+						{
+							entry.title = reader.readElementText();
+						}
+						else if (reader.name() == QLatin1String("link") && reader.attributes().value(QLatin1String("rel")).toString() == QLatin1String("alternate"))
+						{
+							entry.url = QUrl(reader.attributes().value(QLatin1String("href")).toString());
+
+							reader.skipCurrentElement();
+						}
+						else if (reader.name() == QLatin1String("id"))
+						{
+							entry.identifier = reader.readElementText();
+						}
+						else if (reader.name() == QLatin1String("updated"))
+						{
+							entry.updateTime = QDateTime::fromString(reader.readElementText(), Qt::ISODate);
+							entry.updateTime.setTimeSpec(Qt::UTC);
+						}
+						else if (reader.name() == QLatin1String("summary"))
+						{
+							entry.summary = reader.readElementText();
+						}
+						else if (reader.name() == QLatin1String("content"))
+						{
+							entry.content = reader.readElementText(QXmlStreamReader::IncludeChildElements);
+						}
+						else if (reader.name() == QLatin1String("name"))
+						{
+							entry.author = reader.readElementText();
+						}
+						else if (reader.name() == QLatin1String("email"))
+						{
+							entry.email = reader.readElementText();
+						}
+						else if (reader.name() != QLatin1String("author"))
+						{
+							reader.skipCurrentElement();
+						}
+					}
+					else if (reader.isEndElement() && reader.name() == QLatin1String("entry"))
+					{
+						break;
+					}
+				}
+
+				entries.append(entry);
+			}
+			else if (reader.isStartElement())
+			{
+				if (reader.name() == QLatin1String("category"))
+				{
+					categories.append(reader.attributes().value(QLatin1String("term")).toString());
+
+					reader.skipCurrentElement();
+				}
+				else if (reader.name() == QLatin1String("title"))
+				{
+					m_feed->setTitle(reader.readElementText());
+				}
+				else if (reader.name() == QLatin1String("summary"))
+				{
+					m_feed->setDescription(reader.readElementText());
+				}
+				else if (reader.name() == QLatin1String("updated"))
+				{
+					QDateTime updateTime(QDateTime::fromString(reader.readElementText(), Qt::ISODate));
+					updateTime.setTimeSpec(Qt::UTC);
+
+					m_feed->setLastUpdateTime(updateTime);
+				}
+				else
+				{
+					reader.skipCurrentElement();
+				}
+			}
+			else
+			{
+				reader.skipCurrentElement();
+			}
+
+			if (reader.hasError())
+			{
+				Console::addMessage(tr("Failed to parse feed file: %1").arg(reader.errorString()), Console::OtherCategory, Console::ErrorLevel, data->getUrl().toDisplayString());
+			}
+		}
+	}
+
+	entries.squeeze();
+
+	m_feed->setCategories(categories);
+	m_feed->setEntries(entries);
+	m_feed->setLastSynchronizationTime(QDateTime::currentDateTimeUtc());
+
+	return true;
 }
 
 }
