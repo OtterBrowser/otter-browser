67789074e35e8fc4c42326704a7ab80d1b0805ac
Michał Dutkiewicz <emdeck@gmail.com>
2018-06-20 09:49:06 +0200

    Fix warning

--- src/modules/windows/feeds/FeedsContentsWidget.h
+++ src/modules/windows/feeds/FeedsContentsWidget.h
@@ -71,7 +71,7 @@ public:
 	QUrl getUrl() const override;
 	QIcon getIcon() const override;
 	ActionsManager::ActionDefinition::State getActionState(int identifier, const QVariantMap &parameters = {}) const override;
-	bool eventFilter(QObject *object, QEvent *event);
+	bool eventFilter(QObject *object, QEvent *event) override;
 
 public slots:
 	void triggerAction(int identifier, const QVariantMap &parameters = {}, ActionsManager::TriggerType trigger = ActionsManager::UnknownTrigger) override;
