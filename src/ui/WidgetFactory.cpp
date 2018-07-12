103089bf78f98800c1b219d9ac0d3f6be05bc472
Michał Dutkiewicz <emdeck@gmail.com>
2018-07-12 22:12:42 +0200

    Add new module to factory

--- src/ui/WidgetFactory.cpp
+++ src/ui/WidgetFactory.cpp
@@ -48,6 +48,7 @@
 #include "../modules/windows/notes/NotesContentsWidget.h"
 #include "../modules/windows/pageInformation/PageInformationContentsWidget.h"
 #include "../modules/windows/passwords/PasswordsContentsWidget.h"
+#include "../modules/windows/preferences/PreferencesContentsWidget.h"
 #include "../modules/windows/tabHistory/TabHistoryContentsWidget.h"
 #include "../modules/windows/transfers/TransfersContentsWidget.h"
 #include "../modules/windows/web/WebContentsWidget.h"
@@ -245,6 +246,11 @@ ContentsWidget* createContentsWidget(const QString &identifier, const QVariantMa
 	if (identifier == QLatin1String("passwords"))
 	{
 		return new PasswordsContentsWidget(parameters, window, parent);
+	}
+
+	if (identifier == QLatin1String("preferences"))
+	{
+		return new PreferencesContentsWidget(parameters, window, parent);
 	}
 
 	if (identifier == QLatin1String("transfers"))
