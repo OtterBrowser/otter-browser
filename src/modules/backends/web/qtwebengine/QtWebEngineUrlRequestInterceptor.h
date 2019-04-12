a4b995ee0a67a9dfbdc3a0cd03cbea8551e9d991
Michał Dutkiewicz <emdeck@gmail.com>
2019-04-12 21:34:25 +0200

    [QtWebEngine] Initial work on new QtWebEngineUrlRequestInterceptor

--- src/modules/backends/web/qtwebengine/QtWebEngineUrlRequestInterceptor.h
+++ src/modules/backends/web/qtwebengine/QtWebEngineUrlRequestInterceptor.h
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2016 - 2018 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2016 - 2019 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 * Copyright (C) 2016 Jan Bajer aka bajasoft <jbajer@gmail.com>
 *
 * This program is free software: you can redistribute it and/or modify
@@ -21,13 +21,45 @@
 #ifndef OTTER_QTWEBENGINEURLREQUESTINTERCEPTOR_H
 #define OTTER_QTWEBENGINEURLREQUESTINTERCEPTOR_H
 
+#include "../../../../core/NetworkManagerFactory.h"
+
+#if QTWEBENGINECORE_VERSION < 0x050D00
 #include <QtCore/QMap>
 #include <QtCore/QVector>
+#endif
+#include <QtWebEngineCore/QtWebEngineCoreVersion>
 #include <QtWebEngineCore/QWebEngineUrlRequestInterceptor>
 
 namespace Otter
 {
 
+#if QTWEBENGINECORE_VERSION >= 0x050D00
+class QtWebEngineWebWidget;
+
+class QtWebEngineUrlRequestInterceptor final : public QWebEngineUrlRequestInterceptor
+{
+	Q_OBJECT
+
+public:
+	explicit QtWebEngineUrlRequestInterceptor(QtWebEngineWebWidget *parent);
+
+	void interceptRequest(QWebEngineUrlRequestInfo &request) override;
+
+protected:
+	void updateOptions(const QUrl &url);
+	QVariant getOption(int identifier, const QUrl &url) const;
+
+private:
+	QtWebEngineWebWidget *m_widget;
+	QStringList m_unblockedHosts;
+	QVector<int> m_contentBlockingProfiles;
+	NetworkManagerFactory::DoNotTrackPolicy m_doNotTrackPolicy;
+	bool m_areImagesEnabled;
+	bool m_canSendReferrer;
+
+friend class QtWebEngineWebWidget;
+};
+#else
 class QtWebEngineUrlRequestInterceptor final : public QWebEngineUrlRequestInterceptor
 {
 	Q_OBJECT
@@ -51,7 +83,7 @@ private:
 	int m_clearTimer;
 	bool m_areImagesEnabled;
 };
-
+#endif
 }
 
 #endif
