2fb743568c1ac908672bd7bffe86c14870467dca
Michał Dutkiewicz <emdeck@gmail.com>
2019-04-18 22:00:26 +0200

    Declare addon type for color schemes

--- src/core/AddonsManager.h
+++ src/core/AddonsManager.h
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2015 - 2018 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2015 - 2019 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -37,6 +37,7 @@ public:
 	enum AddonType
 	{
 		UnknownType = 0,
+		ColorSchemeType,
 		ExporterType,
 		ExtensionType,
 		ImporterType,
