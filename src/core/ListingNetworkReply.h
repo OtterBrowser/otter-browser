720e8fcff80156877a13a879590772b8cd417939
Michał Dutkiewicz <emdeck@gmail.com>
2019-05-18 23:16:33 +0200

    Fix warning

--- src/core/ListingNetworkReply.h
+++ src/core/ListingNetworkReply.h
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2018 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2018 - 2019 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -56,7 +56,7 @@ protected:
 		QDateTime timeModified;
 		QMimeType mimeType;
 		Type type = UnknownType;
-		quint64 size = 0;
+		qint64 size = 0;
 		bool isSymlink = false;
 	};
 
