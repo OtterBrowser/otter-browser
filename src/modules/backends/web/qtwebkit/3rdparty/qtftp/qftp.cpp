a122c95b1db4484c0ce261db4f9ac6f7cfd0d8e6
Michał Dutkiewicz <emdeck@gmail.com>
2019-08-16 21:41:10 +0200

    [QtWebKit] Use midRef()

--- src/modules/backends/web/qtwebkit/3rdparty/qtftp/qftp.cpp
+++ src/modules/backends/web/qtwebkit/3rdparty/qtftp/qftp.cpp
@@ -951,7 +951,7 @@ void QFtpPI::readyRead()
 
         while (lineLeft4 != endOfMultiLine) {
             if (lineLeft4 == lineCont)
-                replyText += line.mid(4); // strip 'xyz-'
+                replyText += line.midRef(4); // strip 'xyz-'
             else
                 replyText += line;
             if (!commandSocket.canReadLine())
@@ -959,7 +959,7 @@ void QFtpPI::readyRead()
             line = QString::fromLatin1(commandSocket.readLine());
             lineLeft4 = line.left(4);
         }
-        replyText += line.mid(4); // strip reply code 'xyz '
+        replyText += line.midRef(4); // strip reply code 'xyz '
         if (replyText.endsWith(QLatin1String("\r\n")))
             replyText.chop(2);
 
