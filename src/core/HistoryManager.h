c6c92ab31cbc328d3079f876a2b4626e53b0f6a1
Michał Dutkiewicz <emdeck@gmail.com>
2019-10-29 21:31:48 +0100

    Make title and icon optional

--- src/core/HistoryManager.h
+++ src/core/HistoryManager.h
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2013 - 2018 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2013 - 2019 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 * Copyright (C) 2017 Jan Bajer aka bajasoft <jbajer@gmail.com>
 *
 * This program is free software: you can redistribute it and/or modify
@@ -38,14 +38,14 @@ public:
 	static void clearHistory(uint period = 0);
 	static void removeEntry(quint64 identifier);
 	static void removeEntries(const QVector<quint64> &identifiers);
-	static void updateEntry(quint64 identifier, const QUrl &url, const QString &title, const QIcon &icon);
+	static void updateEntry(quint64 identifier, const QUrl &url, const QString &title = {}, const QIcon &icon = {});
 	static HistoryManager* getInstance();
 	static HistoryModel* getBrowsingHistoryModel();
 	static HistoryModel* getTypedHistoryModel();
 	static QIcon getIcon(const QUrl &url);
 	static HistoryModel::Entry* getEntry(quint64 identifier);
 	static QVector<HistoryModel::HistoryEntryMatch> findEntries(const QString &prefix, bool isTypedInOnly = false);
-	static quint64 addEntry(const QUrl &url, const QString &title, const QIcon &icon, bool isTypedIn = false);
+	static quint64 addEntry(const QUrl &url, const QString &title = {}, const QIcon &icon = {}, bool isTypedIn = false);
 	static bool hasEntry(const QUrl &url);
 
 protected:
