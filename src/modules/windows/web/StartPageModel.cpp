97337a46c9db06a830bd4e5d17df88e318cd5b87
Michał Dutkiewicz <emdeck@gmail.com>
2020-01-19 22:01:56 +0100

    Force layout reload after finishing drag

--- src/modules/windows/web/StartPageModel.cpp
+++ src/modules/windows/web/StartPageModel.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2015 - 2019 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2015 - 2020 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -235,6 +235,8 @@ void StartPageModel::handleDragEnded()
 			emit isReloadingTileChanged(index);
 		}
 	}
+
+	emit modelModified();
 }
 
 void StartPageModel::handleBookmarkModified(BookmarksModel::Bookmark *bookmark)
