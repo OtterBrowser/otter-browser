bb013539018ac80c03da7a8852e1357273e24da6
Michał Dutkiewicz <emdeck@gmail.com>
2020-01-30 07:52:57 +0100

    Drop superfluous specifier

--- src/core/NotificationsManager.h
+++ src/core/NotificationsManager.h
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2013 - 2019 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2013 - 2020 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -90,7 +90,7 @@ public:
 			{
 				case ErrorLevel:
 					return ThemesManager::createIcon(QLatin1String("dialog-error"));
-				case Notification::Message::WarningLevel:
+				case WarningLevel:
 					return ThemesManager::createIcon(QLatin1String("dialog-warning"));
 				default:
 					break;
