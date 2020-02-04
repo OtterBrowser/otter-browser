667faa67278fc3b6262136eda9f347fcc3835bd2
Michał Dutkiewicz <emdeck@gmail.com>
2020-02-04 07:40:58 +0100

    Add helper method for marking dialog as accepted

--- src/ui/Dialog.cpp
+++ src/ui/Dialog.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2015 - 2018 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2015 - 2020 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -29,6 +29,7 @@ namespace Otter
 {
 
 Dialog::Dialog(QWidget *parent) : QDialog(parent),
+	m_isAccepted(false),
 	m_wasRestored(false)
 {
 }
@@ -86,6 +87,16 @@ void Dialog::resizeEvent(QResizeEvent *event)
 
 	settings.setObject(settingsObject);
 	settings.save();
+}
+
+void Dialog::markAsAccepted()
+{
+	m_isAccepted = true;
+}
+
+bool Dialog::isAccepted() const
+{
+	return m_isAccepted;
 }
 
 }
--- src/ui/Dialog.h
+++ src/ui/Dialog.h
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2015 - 2018 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2015 - 2020 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -32,11 +32,17 @@ class Dialog : public QDialog
 public:
 	explicit Dialog(QWidget *parent = nullptr);
 
+	bool isAccepted() const;
+
 protected:
 	void showEvent(QShowEvent *event) override;
 	void resizeEvent(QResizeEvent *event) override;
 
+protected slots:
+	void markAsAccepted();
+
 private:
+	bool m_isAccepted;
 	bool m_wasRestored;
 };
 
