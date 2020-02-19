7f1e045e1a4ed607726d2b9077d785680bc5b125
Michał Dutkiewicz <emdeck@gmail.com>
2020-02-19 22:33:18 +0100

    Add signal emitted when parent Window changes

--- src/ui/ContentsWidget.cpp
+++ src/ui/ContentsWidget.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2013 - 2019 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2013 - 2020 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -344,6 +344,8 @@ void ContentsWidget::setParent(Window *window)
 	}
 
 	QWidget::setParent(window);
+
+	emit windowChanged();
 }
 
 ContentsWidget* ContentsWidget::clone(bool cloneHistory) const
--- src/ui/ContentsWidget.h
+++ src/ui/ContentsWidget.h
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2013 - 2019 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2013 - 2020 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -97,6 +97,7 @@ signals:
 	void requestedSearch(const QString &query, const QString &search, SessionsManager::OpenHints hints);
 	void requestedGeometryChange(const QRect &geometry);
 	void webWidgetChanged();
+	void windowChanged();
 	void statusMessageChanged(const QString &message);
 	void titleChanged(const QString &title);
 	void urlChanged(const QUrl &url);
