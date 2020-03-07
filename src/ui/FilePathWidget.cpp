52880931135826ec0ed3fc6e65b2111a2a9a57ae
Michał Dutkiewicz <emdeck@gmail.com>
2020-03-07 22:18:17 +0100

    Simplify

--- src/ui/FilePathWidget.cpp
+++ src/ui/FilePathWidget.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2013 - 2019 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2013 - 2020 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 * Copyright (C) 2016 Piotr Wójcik <chocimier@tlen.pl>
 *
 * This program is free software: you can redistribute it and/or modify
@@ -32,7 +32,7 @@ namespace Otter
 FileSystemCompleterModel::FileSystemCompleterModel(QObject *parent) : QFileSystemModel(parent)
 {
 	setFilter(QDir::NoDotAndDotDot | QDir::AllDirs);
-	setRootPath(QString());
+	setRootPath({});
 }
 
 QVariant FileSystemCompleterModel::data(const QModelIndex &index, int role) const
