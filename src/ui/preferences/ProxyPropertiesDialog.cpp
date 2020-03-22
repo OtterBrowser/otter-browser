48312eb5c82c0e6ac7e8be48f9f161a4931a03bf
Michał Dutkiewicz <emdeck@gmail.com>
2020-03-22 20:35:35 +0100

    Retranslate filters

--- src/ui/preferences/ProxyPropertiesDialog.cpp
+++ src/ui/preferences/ProxyPropertiesDialog.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2017 - 2019 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2017 - 2020 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -113,6 +113,7 @@ void ProxyPropertiesDialog::changeEvent(QEvent *event)
 	if (event->type() == QEvent::LanguageChange)
 	{
 		m_ui->retranslateUi(this);
+		m_ui->automaticConfigurationFilePathWidget->setFilters({tr("PAC files (*.pac)")});
 	}
 }
 
