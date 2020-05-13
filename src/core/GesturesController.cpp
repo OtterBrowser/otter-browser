87d4309d45e82f5fa27c2755713f71541b7b44d9
Michał Dutkiewicz <emdeck@gmail.com>
2020-05-13 21:03:49 +0200

    Introduce GesturesController for easier enablement of gestures on widgets, references #861

---
+++ src/core/GesturesController.cpp
@@ -0,0 +1,86 @@
+/**************************************************************************
+* Otter Browser: Web browser controlled by the user, not vice-versa.
+* Copyright (C) 2020 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+**************************************************************************/
+
+#include "GesturesController.h"
+
+namespace Otter
+{
+
+GesturesController::GesturesController() = default;
+
+GesturesController::~GesturesController() = default;
+
+void GesturesController::installGesturesFilter(QObject *object, GesturesController *target, const QVector<GesturesManager::GesturesContext> &gesturesContexts)
+{
+	new GesturesFilter(object, target);
+
+	m_gesturesContexts = gesturesContexts;
+}
+
+GesturesController::GestureContext GesturesController::getGestureContext(const QPoint &position)
+{
+	Q_UNUSED(position)
+
+	return {{}, m_gesturesContexts};
+}
+
+GesturesFilter::GesturesFilter(QObject *object, GesturesController *target) : QObject(object),
+	m_target(target)
+{
+	object->installEventFilter(this);
+}
+
+bool GesturesFilter::eventFilter(QObject *object, QEvent *event)
+{
+	if (GesturesManager::isTracking())
+	{
+		return QObject::eventFilter(object, event);
+	}
+
+	QPoint position;
+
+	switch (event->type())
+	{
+		case QEvent::MouseButtonDblClick:
+		case QEvent::MouseButtonPress:
+			position = static_cast<QMouseEvent*>(event)->pos();
+
+			break;
+		case QEvent::Wheel:
+			position = static_cast<QWheelEvent*>(event)->pos();
+
+			break;
+		default:
+			break;
+	}
+
+	if (!position.isNull())
+	{
+		const GesturesController::GestureContext context(m_target->getGestureContext(position));
+
+		if (!context.contexts.isEmpty())
+		{
+			GesturesManager::startGesture(parent(), event, context.contexts, context.parameters);
+		}
+	}
+
+	return QObject::eventFilter(object, event);
+}
+
+}
---
+++ src/core/GesturesController.h
@@ -0,0 +1,62 @@
+/**************************************************************************
+* Otter Browser: Web browser controlled by the user, not vice-versa.
+* Copyright (C) 2020 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+**************************************************************************/
+
+#ifndef OTTER_GESTURESCONTROLLER_H
+#define OTTER_GESTURESCONTROLLER_H
+
+#include "GesturesManager.h"
+
+namespace Otter
+{
+
+class GesturesController
+{
+public:
+	struct GestureContext
+	{
+		QVariantMap parameters;
+		QVector<GesturesManager::GesturesContext> contexts;
+	};
+
+	explicit GesturesController();
+	virtual ~GesturesController();
+
+	virtual GestureContext getGestureContext(const QPoint &position = {});
+
+protected:
+	void installGesturesFilter(QObject *object, GesturesController *target, const QVector<GesturesManager::GesturesContext> &gesturesContexts = {});
+
+private:
+	QVector<GesturesManager::GesturesContext> m_gesturesContexts;
+};
+
+class GesturesFilter : public QObject
+{
+public:
+	explicit GesturesFilter(QObject *object, GesturesController *target);
+
+	bool eventFilter(QObject *object, QEvent *event) override;
+
+private:
+	GesturesController *m_target;
+};
+
+}
+
+#endif
