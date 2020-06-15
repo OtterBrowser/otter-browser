9e9563b744a1cd832c050817f07f722923477dcd
Michał Dutkiewicz <emdeck@gmail.com>
2020-06-15 22:16:48 +0200

    Drop nargs

--- packaging/deploy.py
+++ packaging/deploy.py
@@ -129,10 +129,10 @@ def deploy_windows(qt_path, source_path, build_path, target_path):
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Otter Browser deployment tool.')
-	parser.add_argument('--build-path', help='Path to the build directory', nargs=1, default=path.abspath(path.join(path.dirname(sys.argv[0]), '../build')))
-	parser.add_argument('--source-path', help='Path to the source directory', nargs=1, default=path.abspath(path.join(path.dirname(sys.argv[0]), '..')))
-	parser.add_argument('--target-path', help='Path to the output directory', nargs=1, default=path.abspath(path.join(path.dirname(sys.argv[0]), '../output')))
-	parser.add_argument('--qt-path', help='Path to the Qt directory', nargs=1, default=os.getenv('QTDIR', ''))
+	parser.add_argument('--build-path', help='Path to the build directory', default=path.abspath(path.join(path.dirname(sys.argv[0]), '../build')))
+	parser.add_argument('--source-path', help='Path to the source directory', default=path.abspath(path.join(path.dirname(sys.argv[0]), '..')))
+	parser.add_argument('--target-path', help='Path to the output directory', default=path.abspath(path.join(path.dirname(sys.argv[0]), '../output')))
+	parser.add_argument('--qt-path', help='Path to the Qt directory', default=os.getenv('QTDIR', ''))
 
 	arguments = parser.parse_args()
 
