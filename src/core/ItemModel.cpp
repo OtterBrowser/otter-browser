3dc40e89dc538abe712a65d02ec3d4e3851ab1fb
Michał Dutkiewicz <emdeck@gmail.com>
2020-08-13 22:51:11 +0200

    Drop superfluous specifier

--- src/core/ItemModel.cpp
+++ src/core/ItemModel.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2017 - 2019 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2017 - 2020 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -87,7 +87,7 @@ ItemModel::ItemModel(QObject *parent) : QStandardItemModel(parent),
 {
 }
 
-void ItemModel::setupItem(QStandardItem *item, ItemModel::ItemType type)
+void ItemModel::setupItem(QStandardItem *item, ItemType type)
 {
 	item->setData(type, TypeRole);
 
