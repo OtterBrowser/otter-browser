9404503a63d8b6c45307c4715129bfb60a708dd1
Michał Dutkiewicz <emdeck@gmail.com>
2020-10-13 23:03:53 +0200

    Add extra check

--- src/core/CookieJar.cpp
+++ src/core/CookieJar.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2013 - 2019 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2013 - 2020 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -164,7 +164,7 @@ void CookieJar::handleOptionChanged(int identifier, const QVariant &value)
 
 void CookieJar::save()
 {
-	if (SessionsManager::isReadOnly())
+	if (m_isPrivate || SessionsManager::isReadOnly())
 	{
 		return;
 	}
