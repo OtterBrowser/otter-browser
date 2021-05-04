f9f846ee232fdee0eebff2236dbc3d21508eed5c
Michał Dutkiewicz <emdeck@gmail.com>
2021-05-04 23:30:33 +0200

    Drop unused include

--- src/modules/widgets/menuButton/MenuButtonWidget.cpp
+++ src/modules/widgets/menuButton/MenuButtonWidget.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2013 - 2020 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2013 - 2021 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -19,7 +19,6 @@
 
 #include "MenuButtonWidget.h"
 #include "../../../core/SessionsManager.h"
-#include "../../../core/ThemesManager.h"
 #include "../../../ui/MainWindow.h"
 #include "../../../ui/Menu.h"
 #include "../../../ui/ToolBarWidget.h"
