88e918e517f0a9e06dac63fa618ad31e38ed8c44
Michał Dutkiewicz <emdeck@gmail.com>
2021-10-05 19:20:06 +0200

    Declare structure for passing Addon metadata

--- src/core/AddonsManager.h
+++ src/core/AddonsManager.h
@@ -34,6 +34,15 @@ class WebBackend;
 class Addon
 {
 public:
+	struct MetaData
+	{
+		QString title;
+		QString description;
+		QString author;
+		QString version;
+		QUrl homepage;
+	};
+
 	enum AddonType
 	{
 		UnknownType = 0,
