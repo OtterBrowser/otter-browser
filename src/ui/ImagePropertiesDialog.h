68b06d8779aaa26433aa429a2ff086e0cebcea98
Michał Dutkiewicz <emdeck@gmail.com>
2021-12-01 22:42:44 +0100

    Declare enum for image properties

--- src/ui/ImagePropertiesDialog.h
+++ src/ui/ImagePropertiesDialog.h
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2013 - 2020 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2013 - 2021 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -37,6 +37,16 @@ class ImagePropertiesDialog final : public Dialog
 	Q_OBJECT
 
 public:
+	enum ImageProperty
+	{
+		UnknownProperty = 0,
+		AlternativeTextProperty,
+		DepthProperty,
+		HeightProperty,
+		LongDescriptionProperty,
+		WidthProperty
+	};
+
 	explicit ImagePropertiesDialog(const QUrl &url, const QVariantMap &properties, QIODevice *device, QWidget *parent = nullptr);
 	~ImagePropertiesDialog();
 
