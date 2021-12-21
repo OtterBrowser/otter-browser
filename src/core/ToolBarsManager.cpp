349a2c0e17f727bd310ac153a485857a5c18b7c0
Michał Dutkiewicz <emdeck@gmail.com>
2021-12-21 21:57:12 +0100

    Drop old mapping

--- src/core/ToolBarsManager.cpp
+++ src/core/ToolBarsManager.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2015 - 2020 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2015 - 2021 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -241,12 +241,6 @@ void ToolBarsManager::ensureInitialized()
 			for (iterator = localDefinitions.constBegin(); iterator != localDefinitions.constEnd(); ++iterator)
 			{
 				int identifier(getToolBarIdentifier(iterator.key()));
-
-//TODO Drop after migration
-				if (identifier < 0 && iterator.key() == QLatin1String("NavigationBar") && !localDefinitions.contains(QLatin1String("AddressBar")))
-				{
-					identifier = AddressBar;
-				}
 
 				if (identifier >= 0)
 				{
