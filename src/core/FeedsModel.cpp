5dcc2599db127bf7035fe348e94b04c9aedefc98
Michał Dutkiewicz <emdeck@gmail.com>
2022-02-26 22:59:56 +0100

    Simplify

--- src/core/FeedsModel.cpp
+++ src/core/FeedsModel.cpp
@@ -139,7 +139,7 @@ QVariant FeedsModel::Entry::data(int role) const
 			case UpdateIntervalRole:
 				return m_feed->getUpdateInterval();
 			case UpdateProgressValueRole:
-				return (m_feed->isUpdating() ? m_feed->getUpdateProgress() : 100);
+				return m_feed->getUpdateProgress();
 			case IsShowingProgressIndicatorRole:
 				return (m_feed->isUpdating() || (m_feed->getLastSynchronizationTime().isValid() && m_feed->getLastSynchronizationTime().msecsTo(QDateTime::currentDateTimeUtc()) < 2500));
 			case IsUpdatingRole:
