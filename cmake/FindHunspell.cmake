5ad719ddd58eba33e5adeaa0e6832469ae3197d1
Michał Dutkiewicz <emdeck@gmail.com>
2022-05-03 22:36:44 +0200

    Normalize case

--- cmake/FindHunspell.cmake
+++ cmake/FindHunspell.cmake
@@ -6,17 +6,17 @@
 #  Hunspell_LIBRARIES - The libraries needed to use Hunspell
 #  Hunspell_DEFINITIONS - Compiler switches required for using Hunspell
 
-IF (Hunspell_INCLUDE_DIR AND Hunspell_LIBRARIES)
-  # Already in cache, be silent
-  SET(Hunspell_FIND_QUIETLY TRUE)
-ENDIF (Hunspell_INCLUDE_DIR AND Hunspell_LIBRARIES)
+if (Hunspell_INCLUDE_DIR AND Hunspell_LIBRARIES)
+    # Already in cache, be silent
+    set(Hunspell_FIND_QUIETLY TRUE)
+endif (Hunspell_INCLUDE_DIR AND Hunspell_LIBRARIES)
 
-FIND_PATH(Hunspell_INCLUDE_DIR hunspell/hunspell.hxx)
-FIND_LIBRARY(Hunspell_LIBRARIES NAMES hunspell-1.7 hunspell-1.6 hunspell-1.5 hunspell)
+find_path(Hunspell_INCLUDE_DIR hunspell/hunspell.hxx)
+find_library(Hunspell_LIBRARIES NAMES hunspell-1.7 hunspell-1.6 hunspell-1.5 hunspell)
 
 # handle the QUIETLY and REQUIRED arguments and set Hunspell_FOUND to TRUE if
 # all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Hunspell DEFAULT_MSG Hunspell_LIBRARIES Hunspell_INCLUDE_DIR)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Hunspell DEFAULT_MSG Hunspell_LIBRARIES Hunspell_INCLUDE_DIR)
 
-MARK_AS_ADVANCED(Hunspell_INCLUDE_DIR Hunspell_LIBRARIES)
+mark_as_advanced(Hunspell_INCLUDE_DIR Hunspell_LIBRARIES)
