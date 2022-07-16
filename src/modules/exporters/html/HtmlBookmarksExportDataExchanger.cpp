237f10819b3c0db1b511c65db20562f3379a0837
Michał Dutkiewicz <emdeck@gmail.com>
2022-07-16 21:43:42 +0200

    Add stub of bookmarks exporter for HTML format, references #726

---
+++ src/modules/exporters/html/HtmlBookmarksExportDataExchanger.cpp
@@ -0,0 +1,88 @@
+/**************************************************************************
+* Otter Browser: Web browser controlled by the user, not vice-versa.
+* Copyright (C) 2022 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+**************************************************************************/
+
+#include "HtmlBookmarksExportDataExchanger.h"
+
+#include <QtCore/QDir>
+
+namespace Otter
+{
+
+HtmlBookmarksExportDataExchanger::HtmlBookmarksExportDataExchanger(QObject *parent) : ExportDataExchanger(parent)
+{
+}
+
+QString HtmlBookmarksExportDataExchanger::getName() const
+{
+	return QLatin1String("html");
+}
+
+QString HtmlBookmarksExportDataExchanger::getTitle() const
+{
+	return tr("Export HTML Bookmarks");
+}
+
+QString HtmlBookmarksExportDataExchanger::getDescription() const
+{
+	return tr("Exports bookmarks to HTML file (Netscape format).");
+}
+
+QString HtmlBookmarksExportDataExchanger::getVersion() const
+{
+	return QLatin1String("1.0");
+}
+
+QString HtmlBookmarksExportDataExchanger::getSuggestedPath(const QString &path) const
+{
+	if (!path.isEmpty() && QFileInfo(path).isDir())
+	{
+		return QDir(path).filePath(QLatin1String("bookmarks.html"));
+	}
+
+	return path;
+}
+
+QString HtmlBookmarksExportDataExchanger::getGroup() const
+{
+	return QLatin1String("other");
+}
+
+QUrl HtmlBookmarksExportDataExchanger::getHomePage() const
+{
+	return QUrl(QLatin1String("https://otter-browser.org/"));
+}
+
+QStringList HtmlBookmarksExportDataExchanger::getFileFilters() const
+{
+	return {tr("HTML files (*.htm *.html)")};
+}
+
+DataExchanger::ExchangeType HtmlBookmarksExportDataExchanger::getExchangeType() const
+{
+	return BookmarksExchange;
+}
+
+bool HtmlBookmarksExportDataExchanger::exportData(const QString &path)
+{
+	Q_UNUSED(path)
+
+	return false;
+}
+
+}
---
+++ src/modules/exporters/html/HtmlBookmarksExportDataExchanger.h
@@ -0,0 +1,51 @@
+/**************************************************************************
+* Otter Browser: Web browser controlled by the user, not vice-versa.
+* Copyright (C) 2022 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+**************************************************************************/
+
+#ifndef OTTER_HTMLBOOKMARKSEXPORTDATAEXCHANGER_H
+#define OTTER_HTMLBOOKMARKSEXPORTDATAEXCHANGER_H
+
+#include "../../../core/DataExchanger.h"
+
+namespace Otter
+{
+
+class HtmlBookmarksExportDataExchanger final : public ExportDataExchanger
+{
+	Q_OBJECT
+
+public:
+	explicit HtmlBookmarksExportDataExchanger(QObject *parent = nullptr);
+
+	QString getName() const override;
+	QString getTitle() const override;
+	QString getDescription() const override;
+	QString getVersion() const override;
+	QString getSuggestedPath(const QString &path = {}) const override;
+	QString getGroup() const override;
+	QUrl getHomePage() const override;
+	QStringList getFileFilters() const override;
+	ExchangeType getExchangeType() const override;
+
+public slots:
+	bool exportData(const QString &path) override;
+};
+
+}
+
+#endif
