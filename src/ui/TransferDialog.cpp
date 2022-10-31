f9dbab7cf11df3e6eafb12bd1e06ff8fd36bb8cf
Michał Dutkiewicz <emdeck@gmail.com>
2022-10-31 22:05:37 +0100

    Drop no longer used header

--- src/ui/TransferDialog.cpp
+++ src/ui/TransferDialog.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2015 - 2019 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2015 - 2022 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -26,7 +26,6 @@
 #include "ui_TransferDialog.h"
 
 #include <QtCore/QFileInfo>
-#include <QtCore/QtMath>
 
 namespace Otter
 {
