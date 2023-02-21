48b79ee058a01822ae12ccc052eacbb590cbbb28
Michał Dutkiewicz <emdeck@gmail.com>
2023-02-21 22:52:01 +0100

    Drop negation

--- src/ui/Dialog.cpp
+++ src/ui/Dialog.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2015 - 2021 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2015 - 2023 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -40,11 +40,7 @@ void Dialog::showEvent(QShowEvent *event)
 	{
 		QFile file(SessionsManager::getWritableDataPath(QLatin1String("dialogs.json")));
 
-		if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
-		{
-			adjustSize();
-		}
-		else
+		if (file.open(QIODevice::ReadOnly | QIODevice::Text))
 		{
 			const QString name(Utils::normalizeObjectName(objectName(), QLatin1String("Dialog")));
 
@@ -64,6 +60,10 @@ void Dialog::showEvent(QShowEvent *event)
 			}
 
 			file.close();
+		}
+		else
+		{
+			adjustSize();
 		}
 
 		m_wasRestored = true;
