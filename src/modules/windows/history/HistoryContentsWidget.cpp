1a55b336206eea16f1c61ee33f87fe2eeb6afc27
Michał Dutkiewicz <emdeck@gmail.com>
2023-03-11 23:02:20 +0100

    Do not check twice if action is valid

--- src/modules/windows/history/HistoryContentsWidget.cpp
+++ src/modules/windows/history/HistoryContentsWidget.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2013 - 2022 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2013 - 2023 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -239,7 +239,7 @@ void HistoryContentsWidget::openEntry()
 
 		if (action && mainWindow)
 		{
-			mainWindow->triggerAction(ActionsManager::OpenUrlAction, {{QLatin1String("url"), url}, {QLatin1String("hints"), QVariant(action ? static_cast<SessionsManager::OpenHints>(action->data().toInt()) : SessionsManager::DefaultOpen)}});
+			mainWindow->triggerAction(ActionsManager::OpenUrlAction, {{QLatin1String("url"), url}, {QLatin1String("hints"), QVariant(static_cast<SessionsManager::OpenHints>(action->data().toInt()))}});
 		}
 	}
 }
