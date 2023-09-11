b594e7d3d685ccbc10671eb7e273f8e49869736b
Michał Dutkiewicz <emdeck@gmail.com>
2023-09-11 22:21:55 +0200

    Return early

--- src/modules/widgets/address/AddressCompletionModel.h
+++ src/modules/widgets/address/AddressCompletionModel.h
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2013 - 2020 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2013 - 2023 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 * Copyright (C) 2016 - 2017 Jan Bajer aka bajasoft <jbajer@gmail.com>
 * Copyright (C) 2016 Piotr Wójcik <chocimier@tlen.pl>
 *
