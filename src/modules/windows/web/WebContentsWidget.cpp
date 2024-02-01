c6f48eda4d786187b5b95ceca4c43ccb76c1a253
Michał Dutkiewicz <emdeck@gmail.com>
2024-02-01 23:11:21 +0100

    Return early

--- src/modules/windows/web/WebContentsWidget.cpp
+++ src/modules/windows/web/WebContentsWidget.cpp
@@ -339,39 +339,41 @@ void WebContentsWidget::triggerAction(int identifier, const QVariantMap &paramet
 			{
 				const QString text(m_webWidget->getSelectedText().trimmed());
 
-				if (!text.isEmpty())
+				if (text.isEmpty())
 				{
-					const InputInterpreter::InterpreterResult result(InputInterpreter::interpret(text, (InputInterpreter::NoBookmarkKeywordsFlag | InputInterpreter::NoHostLookupFlag | InputInterpreter::NoSearchKeywordsFlag)));
+					return;
+				}
 
-					if (result.isValid())
+				const InputInterpreter::InterpreterResult result(InputInterpreter::interpret(text, (InputInterpreter::NoBookmarkKeywordsFlag | InputInterpreter::NoHostLookupFlag | InputInterpreter::NoSearchKeywordsFlag)));
+
+				if (result.isValid())
+				{
+					const SessionsManager::OpenHints hints(parameters.contains(QLatin1String("hints")) ? SessionsManager::calculateOpenHints(parameters) : SessionsManager::calculateOpenHints());
+
+					switch (result.type)
 					{
-						const SessionsManager::OpenHints hints(parameters.contains(QLatin1String("hints")) ? SessionsManager::calculateOpenHints(parameters) : SessionsManager::calculateOpenHints());
+						case InputInterpreter::InterpreterResult::UrlType:
+							if (hints.testFlag(SessionsManager::CurrentTabOpen) && hints.testFlag(SessionsManager::PrivateOpen) == isPrivate())
+							{
+								setUrl(result.url);
+							}
+							else
+							{
+								MainWindow *mainWindow(MainWindow::findMainWindow(this));
 
-						switch (result.type)
-						{
-							case InputInterpreter::InterpreterResult::UrlType:
-								if (hints.testFlag(SessionsManager::CurrentTabOpen) && hints.testFlag(SessionsManager::PrivateOpen) == isPrivate())
+								if (mainWindow)
 								{
-									setUrl(result.url);
+									mainWindow->triggerAction(ActionsManager::OpenUrlAction, {{QLatin1String("url"), result.url}, {QLatin1String("hints"), QVariant(hints)}}, trigger);
 								}
-								else
-								{
-									MainWindow *mainWindow(MainWindow::findMainWindow(this));
+							}
 
-									if (mainWindow)
-									{
-										mainWindow->triggerAction(ActionsManager::OpenUrlAction, {{QLatin1String("url"), result.url}, {QLatin1String("hints"), QVariant(hints)}}, trigger);
-									}
-								}
+							break;
+						case InputInterpreter::InterpreterResult::SearchType:
+							emit requestedSearch(result.searchQuery, result.searchEngine, hints);
 
-								break;
-							case InputInterpreter::InterpreterResult::SearchType:
-								emit requestedSearch(result.searchQuery, result.searchEngine, hints);
-
-								break;
-							default:
-								break;
-						}
+							break;
+						default:
+							break;
 					}
 				}
 			}
