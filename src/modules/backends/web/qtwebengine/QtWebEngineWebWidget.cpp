dce6aebd00e606369d3c3fd318fd47ed7acddcaa
Jimi Huotari <chiitoo@gentoo.org>
2024-07-04 01:07:34 +0300

    qt6: include 'QtWebEngineCore/QWebEngineHttpRequest'

--- src/modules/backends/web/qtwebengine/QtWebEngineWebWidget.cpp
+++ src/modules/backends/web/qtwebengine/QtWebEngineWebWidget.cpp
@@ -54,6 +54,7 @@
 #include <QtWebEngineCore/QWebEngineCookieStore>
 #include <QtWebEngineCore/QWebEngineFindTextResult>
 #include <QtWebEngineCore/QWebEngineHistory>
+#include <QtWebEngineCore/QWebEngineHttpRequest>
 #include <QtWebEngineCore/QWebEngineProfile>
 #include <QtWebEngineCore/QWebEngineScript>
 #include <QtWebEngineCore/QWebEngineSettings>
