05c757b778c68388dff65cde13ea6c1a3e898197
Michał Dutkiewicz <emdeck@gmail.com>
2024-08-13 21:56:32 +0200

    Use helper function

--- src/ui/ContentFiltersViewWidget.cpp
+++ src/ui/ContentFiltersViewWidget.cpp
@@ -905,10 +905,7 @@ void ContentFiltersViewWidget::save()
 		}
 	}
 
-	for (int i = 0; i < m_filesToRemove.count(); ++i)
-	{
-		QFile::remove(m_filesToRemove.at(i));
-	}
+	Utils::removeFiles(m_filesToRemove);
 
 	m_filesToRemove.clear();
 
