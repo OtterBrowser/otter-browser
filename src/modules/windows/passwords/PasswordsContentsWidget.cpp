8fed0e5344cd4f1cc0032436ba0ed1f8ffab0e7c
Michał Dutkiewicz <emdeck@gmail.com>
2025-05-22 21:55:19 +0200

    Use single QString::arg() call

--- src/modules/windows/passwords/PasswordsContentsWidget.cpp
+++ src/modules/windows/passwords/PasswordsContentsWidget.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2016 - 2024 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2016 - 2025 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -201,7 +201,7 @@ void PasswordsContentsWidget::populatePasswords()
 			hostItem->appendRow({setItem, new QStandardItem()});
 		}
 
-		hostItem->setText(QStringLiteral("%1 (%2)").arg(host).arg(hostItem->rowCount()));
+		hostItem->setText(QStringLiteral("%1 (%2)").arg(host, QString::number(hostItem->rowCount())));
 
 		m_model->appendRow(hostItem);
 
