222df9bbd5bec106e4133709284dccd05a911aa8
Michał Dutkiewicz <emdeck@gmail.com>
2025-11-14 20:51:26 +0100

    Drop unused include

--- src/ui/TransferDialog.cpp
+++ src/ui/TransferDialog.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2015 - 2022 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2015 - 2025 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -19,7 +19,6 @@
 
 #include "TransferDialog.h"
 #include "../core/HandlersManager.h"
-#include "../core/ThemesManager.h"
 #include "../core/TransfersManager.h"
 #include "../core/Utils.h"
 
