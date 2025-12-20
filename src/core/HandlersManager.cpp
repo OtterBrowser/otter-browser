f9ab8cef474ce6b5f50d42afe35791fd6e86399c
Michał Dutkiewicz <emdeck@gmail.com>
2025-12-20 23:29:25 +0100

    Drop unused include

--- src/core/HandlersManager.cpp
+++ src/core/HandlersManager.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2015 - 2023 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2015 - 2025 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -23,7 +23,6 @@
 #include "IniSettings.h"
 #include "SessionsManager.h"
 #include "SettingsManager.h"
-#include "Utils.h"
 
 #include <QtCore/QFile>
 #include <QtCore/QMimeDatabase>
