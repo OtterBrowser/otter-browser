a4f39787f5fec450de42ed18713241b68fb8a19d
Michał Dutkiewicz <emdeck@gmail.com>
2026-02-05 23:00:45 +0100

    Mark as static

--- src/core/ActionExecutor.cpp
+++ src/core/ActionExecutor.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2017 - 2025 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2017 - 2026 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -130,7 +130,7 @@ ActionsManager::ActionDefinition::State ActionExecutor::Object::getActionState(i
 	return state;
 }
 
-QMetaMethod ActionExecutor::Object::getMethod(const QMetaObject *metaObject, const char *method) const
+QMetaMethod ActionExecutor::Object::getMethod(const QMetaObject *metaObject, const char *method)
 {
 	return metaObject->method(metaObject->indexOfSignal(method));
 }
--- src/core/ActionExecutor.h
+++ src/core/ActionExecutor.h
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2017 - 2025 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2017 - 2026 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -47,7 +47,7 @@ public:
 		bool isValid() const;
 
 	protected:
-		QMetaMethod getMethod(const QMetaObject *metaObject, const char *method) const;
+		static QMetaMethod getMethod(const QMetaObject *metaObject, const char *method);
 
 	private:
 		QPointer<QObject> m_object;
