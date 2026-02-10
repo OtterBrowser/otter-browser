cbaa58dc96a80aa0a40797421ea5610efe482c9d
Michał Dutkiewicz <emdeck@gmail.com>
2026-02-10 22:40:05 +0100

    Fix Qt6 warning

--- src/ui/ContentBlockingProfileDialog.cpp
+++ src/ui/ContentBlockingProfileDialog.cpp
@@ -1,7 +1,7 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
 * Copyright (C) 2016 Jan Bajer aka bajasoft <jbajer@gmail.com>
-* Copyright (C) 2016 - 2025 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2016 - 2026 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -156,7 +156,7 @@ void ContentBlockingProfileDialog::closeEvent(QCloseEvent *event)
 {
 	if (m_ui->saveButton->isEnabled())
 	{
-		const int result(QMessageBox::question(this, tr("Question"), tr("The source has been modified.\nDo you want to save it?"), QMessageBox::Yes, QMessageBox::No, QMessageBox::Cancel));
+		const int result(QMessageBox::question(this, tr("Question"), tr("The source has been modified.\nDo you want to save it?"), (QMessageBox::Yes | QMessageBox::No | QMessageBox::Cancel)));
 
 		if (result == QMessageBox::Cancel)
 		{
