9dc2d9cca4ca549c5c3b0693392a82b0c1764ac3
Michał Dutkiewicz <emdeck@gmail.com>
2026-02-26 21:56:09 +0100

    Move to range based for

--- src/ui/ProxyModel.cpp
+++ src/ui/ProxyModel.cpp
@@ -1,7 +1,7 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
 * Copyright (C) 2015 Piotr Wójcik <chocimier@tlen.pl>
-* Copyright (C) 2015 - 2025 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2015 - 2026 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -38,9 +38,8 @@ QMimeData* ProxyModel::mimeData(const QModelIndexList &indexes) const
 	QModelIndexList sourceIndexes;
 	sourceIndexes.reserve(indexes.count());
 
-	for (int i = 0; i < indexes.count(); ++i)
+	for (const QModelIndex index: indexes)
 	{
-		const QModelIndex index(indexes.at(i));
 		const QModelIndex sourceIndex(mapToSource(index.sibling(index.row(), 0)));
 
 		if (!sourceIndexes.contains(sourceIndex))
