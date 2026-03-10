bc982ee40faa6001d3e55c94d52c13d4c334e69c
Michał Dutkiewicz <emdeck@gmail.com>
2026-03-10 21:38:33 +0100

    Move to range based for

--- src/ui/ApplicationComboBoxWidget.cpp
+++ src/ui/ApplicationComboBoxWidget.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2015 - 2025 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2015 - 2026 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -130,10 +130,8 @@ void ApplicationComboBoxWidget::setMimeType(const QMimeType &mimeType)
 
 	if (!onlyDefaultApplication)
 	{
-		for (int i = 0; i < applications.count(); ++i)
+		for (const ApplicationInformation &application: applications)
 		{
-			const ApplicationInformation application(applications.at(i));
-
 			addItem(application.icon, ((application.name.isEmpty()) ? tr("Unknown") : application.name), application.command);
 
 			if (application.icon.isNull())
