056a503747d01f909528933f4930ff8ffe346adc
Michał Dutkiewicz <emdeck@gmail.com>
2026-03-16 19:21:19 +0100

    Use helper variable

--- src/ui/StartupDialog.cpp
+++ src/ui/StartupDialog.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2013 - 2025 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2013 - 2026 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -48,7 +48,9 @@ StartupDialog::StartupDialog(const QString &sessionName, QWidget *parent) : Dial
 
 	for (int i = 0; i < sessions.count(); ++i)
 	{
-		m_ui->sessionComboBox->addItem((sessions.at(i).title.isEmpty() ? tr("(Untitled)") : sessions.at(i).title), sessions.at(i).path);
+		const SessionInformation session(sessions.at(i));
+
+		m_ui->sessionComboBox->addItem((session.title.isEmpty() ? tr("(Untitled)") : session.title), session.path);
 	}
 
 	const int index(qMax(0, m_ui->sessionComboBox->findData(sessionName)));
