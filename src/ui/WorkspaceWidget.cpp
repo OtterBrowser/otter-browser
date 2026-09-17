5c9faf97e3702e361710ef145fe262e667b8a776
Michał Dutkiewicz <emdeck@gmail.com>
2026-09-17 23:29:39 +0200

    More qOverload porting

--- src/ui/WorkspaceWidget.cpp
+++ src/ui/WorkspaceWidget.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 * Otter Browser: Web browser controlled by the user, not vice-versa.
-* Copyright (C) 2013 - 2025 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
+* Copyright (C) 2013 - 2026 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -703,7 +703,7 @@ void WorkspaceWidget::setActiveWindow(Window *window, bool force)
 					subWindow->raise();
 				}
 
-				QTimer::singleShot(0, subWindow, static_cast<void(MdiWindow::*)()>(&MdiWindow::setFocus));
+				QTimer::singleShot(0, subWindow, qOverload<>(&MdiWindow::setFocus));
 			}
 		}
 
