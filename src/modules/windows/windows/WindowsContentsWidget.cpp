79b8b621ce7a56047fa58c4e422b177aa6e9e30c
Michał Dutkiewicz <emdeck@gmail.com>
2026-09-22 22:14:16 +0200

    Move to qOverload

--- src/modules/windows/windows/WindowsContentsWidget.cpp
+++ src/modules/windows/windows/WindowsContentsWidget.cpp
@@ -133,7 +133,7 @@ WindowsContentsWidget::WindowsContentsWidget(const QVariantMap &parameters, Wind
 	{
 		m_ui->windowsViewWidget->setExpanded(index, true);
 	});
-	connect(SessionsManager::getModel(), &SessionModel::modelModified, m_ui->windowsViewWidget->viewport(), static_cast<void(QWidget::*)()>(&QWidget::update));
+	connect(SessionsManager::getModel(), &SessionModel::modelModified, m_ui->windowsViewWidget->viewport(), qOverload<>(&QWidget::update));
 	connect(m_ui->filterLineEditWidget, &LineEditWidget::textChanged, m_ui->windowsViewWidget, &ItemViewWidget::setFilterString);
 	connect(m_ui->windowsViewWidget, &ItemViewWidget::customContextMenuRequested, this, &WindowsContentsWidget::showContextMenu);
 	connect(m_ui->windowsViewWidget, &ItemViewWidget::clicked, this, &WindowsContentsWidget::activateWindow);
